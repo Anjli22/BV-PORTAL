76e84bf89bba86b1617a427f98d9e98458ddaead
unknown <kushwahanjali037@gmail.com>
2022-03-05 18:36:38 +0530

    first commit

---
+++ Student/html/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/login.css">
+    <link rel="shortcut icon" href="https://tse2.mm.bing.net/th?id=OIP.TRLdZgnfAkaU15U8ICMdZAHaGG&pid=Api&P=0&w=194&h=161" type="image/x-icon">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <title>Banasthali Portal</title>
+</head>
+<body>
+    <div class="main">
+        <div class="heading"><p>BANASTHALI PORTAL</p></div>
+        
+        <div class="toggle">
+            <div class="toggle-btn">
+                <button id="student-btn" class="active" onclick="showlogin();">Student</button>
+            </div>
+            <div class="toggle-btn">
+                <button id="teacher-btn" onclick="showsignup();">Teacher</button>
+            </div>
+       
+        </div>
+        <div></div>
+            
+    </div>
+</body>
+</html>
